cfd637ccbae2e6efe608ab46e9d2d2857724a9c9
VanacV <vanya_ev@mail.ru>
2024-10-31 00:16:48 +0300

    Fix_CORS_AHHAHAH2

---
+++ src/main/java/com/code_vanac/fullstack_backend/WebConfig.java
@@ -0,0 +1,17 @@
+package com.code_vanac.fullstack_backend;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Применяется ко всем путям
+                .allowedOrigins("https://ci-cdfrontend-production.up.railway.app") // Разрешенный домен
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Разрешенные методы
+                .allowCredentials(true);
+    }
+}
