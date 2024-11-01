9bba9686d7737b602f0e2f01e25ad32273cd923f
VanacV <vanya_ev@mail.ru>
2024-11-01 12:31:31 +0300

    add proxy3

--- src/main/java/com/code_vanac/fullstack_backend/controller/UserController.java
+++ src/main/java/com/code_vanac/fullstack_backend/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "https://ci-cdfrontend-production.up.railway.app")
 public class UserController {
 
     @Autowired
